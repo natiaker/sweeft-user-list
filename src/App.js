0c5c807d7024e8e5240c85d4ced062dd551cf208
natiaker <n.kerdzaia@gmail.com>
2023-03-16 19:14:02 +0400

    feat: add infinit scroll to the user list

--- src/App.js
+++ src/App.js
@@ -6,34 +6,53 @@ const baseUrl = "http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com";
 
 function App() {
   const [userList, setUserList] = useState(null);
-  const [page, setPage] = useState(1);
-  const [size, setSize] = useState(5);
+  const [page, setPage] = useState(2);
+  const size = 100;
   const [isLoading, setIsLoading] = useState(false);
+  const [noData, setNoData] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [url, setUrl] = useState(`${baseUrl}/user/${page}/${size}/`);
 
   useEffect(() => {
-    setIsLoading(true);
     try {
-      axios.get(url).then(response => {
+      axios.get(`${baseUrl}/user/1/${size}/`).then(response => {
         setIsError(false);
-        setUserList(response.data);
+        setUserList(response.data.list);
       });
     } catch (error) {
       console.log("Error fetching data: ", error);
       setIsError(true);
-    } finally {
-      setIsLoading(false);
     }
-  }, [url]);
+  }, []);
+
+  window.onscroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      if (!noData) {
+        setIsLoading(true);
+        try {
+          axios.get(`${baseUrl}/user/${page}/${size}/`).then(response => {
+            setIsError(false);
+            const newPage = page + 1;
+            setPage(newPage);
+            const newList = userList.concat(response.data.list);
+            setUserList(newList);
+            console.log(newList);
+            if (response.data.list.length === 0) setNoData(true);
+          });
+        } catch (error) {
+          console.log("Error fetching data: ", error);
+          setIsError(true);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    }
+  };
 
   if (!userList) return null;
 
-  isLoading && (
-    <div className='container'>
-      <h1>Loading...</h1>
-    </div>
-  );
   isError && (
     <div className='container'>
       <h1>error happend</h1>
@@ -41,10 +60,16 @@ function App() {
   );
 
   return (
-    <div className='container'>
+    <div
+      className='container'
+      style={{ width: "1000px" }}
+    >
       <div className='users'>
-        <div className='list'>
-          {userList.list.map(listItem => {
+        <div
+          className='list'
+          style={{ display: "flex", flexWrap: "wrap" }}
+        >
+          {userList.map(listItem => {
             return (
               <ListItem
                 key={listItem.id}
@@ -54,6 +79,8 @@ function App() {
           })}
         </div>
       </div>
+      {isLoading && <h1>Loading...</h1>}
+      {noData && <h1>No more data</h1>}
     </div>
   );
 }
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const ListItem = ({ name, lastName, prefix, title, imageUrl }) => {
   return (
-    <div className='user-container'>
+    <div
+      className='user-container'
+      style={{ border: "2px solid red", width: "200px" }}
+    >
       <img
         src={imageUrl}
         alt={`${name} ${lastName}`}
+        style={{ width: "200px" }}
       />
       <div>
         <h1>
